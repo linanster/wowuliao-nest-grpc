2bd990dc13f517f0eecda4da402e1e7ce87ce5f0
Nan Li <nan.li@on-running.com>
2025-02-25 11:20:49 +0800

    Update g-user.service.ts

--- apps/g-user/src/g-user.service.ts
+++ apps/g-user/src/g-user.service.ts
@@ -1,11 +1,14 @@
 import { Injectable } from '@nestjs/common';
 
+const os = require('os');
+
 @Injectable()
 export class GUserService {
   getUser(id: string): { id: string; name: string } {
     return {
       id,
       name: 'John Doe',
+      hostname: os.hostname()
     };
   }
 }
